76cfa4a4a53a1bd038c538fa85b6dd827ab401e5
Davi Barci <davibarci0208@gmail.com>
2023-05-02 14:35:43 -0300

    feat: confirm email

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -4,5 +4,6 @@ import orders from "../controllers/orders.controller.js";
 const ordersRoutes = Router();
 
 ordersRoutes.post("/checkout", orders.create);
+ordersRoutes.post("/confirmemail", orders.sendEmailConfirm);
 
 export default ordersRoutes;
